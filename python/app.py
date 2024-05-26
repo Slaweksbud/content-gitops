b1ba16bf0ea512670e0eb1daf3a232c76c801f51
Slaweksbud <37973865+Slaweksbud@users.noreply.github.com>
2024-05-26 12:40:58 +0200

    v12

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World - slaweknew"
+    return "Hello World - slaweknewv12"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
